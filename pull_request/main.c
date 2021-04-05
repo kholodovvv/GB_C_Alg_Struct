601ec524debcb054c43b21698b8e1c9eabf4ed96
kholodovvv <apersand@mail.ru>
2021-04-05 20:50:55 +1000

    pull_request Homework for lesson12

--- pull_request/main.c
+++ pull_request/main.c
@@ -1,131 +1,239 @@
 #include <stdio.h>
-#include <math.h>
-#include <time.h>
+#include <stdlib.h>
 
-#define WIDTH_BOARD 8
-#define HEIGHT_BOARD 8
-#define IMPEDIMENT 1
+#define true 1 == 1
+#define false 1 != 1
 
-int BOARD[WIDTH_BOARD][HEIGHT_BOARD];
+typedef int boolean;
 
-void getBinaryView(int num){
+typedef struct Node{
+    int key;
+    struct Node *left;
+    struct Node *right;
+}TreeNode;
 
-	if(num >= 2){
+
+TreeNode *treeInsert(TreeNode *t, int data){
+
+TreeNode *newNode = malloc(sizeof(TreeNode));
+
+    newNode->key = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+TreeNode *current = t;
+TreeNode *parent = t;
+
+    if(t == NULL){
+        t = newNode;
+    }else{
+        while (current->key != data)
+        {
+            parent = current;
+
+            if(current->key > data){
+                current = current->left;
+
+                if(current == NULL){
+                    parent->left = newNode;
+
+                    return t;
+                }
+            }else{
+                current = current->right;
+
+                if(current == NULL){
+                    parent->right = newNode;
+
+                    return t;
+                }
+            }
+        }
+        
+    }
+    return t;
+}
+
+
+
+boolean isBalanced(TreeNode *root){
+	
+	TreeNode *lTree = root->left;
+	TreeNode *rTree = root->right;
+	TreeNode *lNode = root;
+	TreeNode *rNode = root;
+
+	int lcount = 0, rcount = 0; 
+	
+	if(root){//посещаем корень
+
+	while(lTree != NULL){
 		
-		getBinaryView(num / 2);
-	}
+		lNode = lTree;
 		
-	printf("%d", num % 2);
-}
-
-double getExpo(double number, int degree){
-	
-	if(degree == 0) return 1.0;
-	if(degree < 0) return getExpo(1.0 / number, -degree);
-	if(degree % 2) return number * getExpo(number, degree - 1);
-	
-	return getExpo(number * number, degree / 2);
-}
-
-double expo(double n, int deg){
-	
-	if(deg % 2 == 0){
-
-		 return expo(n * n, deg / 2);
-	 }else{
-		 
-		 return n * pow(n, deg - 1);
-	 }
-	
-	return pow(n, deg);
-}
-
-void resetArray(){
-	
-	for(int i = 0; i < WIDTH_BOARD; ++i)
-	for(int j = 0; j < HEIGHT_BOARD; ++j)
-		BOARD[i][j] = 0;
-}
-
-void setImpediment(){
-	
-	srand(time(NULL));
-	BOARD[1 + (rand()%8)][1 + (rand()%8)] = IMPEDIMENT;
-}
-
-
-int stepKing(int x, int y){
-	
-	if(x == 0 && y == 0)
-		return 0;
-	else if(BOARD[x][y] == 1)
-		return 0;
-	else if(x == 0 ^ y == 0)
-		return 1;
-	else 
-	return stepKing(x, y - 1) + stepKing(x - 1, y);
-
-}
-
-int main(const int argv, const char **argc){
-
-//----------------------- Task 1 ----------------------------------
-int number = 0;
-
-	do{
-		printf("ENTER NUMBER MORE 0: ");
-		scanf("%d", &number);
-	}while(number < 0);
-
-getBinaryView(number);
-//-----------------------------------------------------------------
-printf("\n");
-//----------------------- Task 2 ----------------------------------
-int degr = 0;
-double num = 0.0;	
-	
-	
-printf("ENTER THE NUMBER AND THE DEGREE OF THE NUMBER SEPARATED BY A SPACE: ");
-scanf("%lf %d", &num, &degr);
-
-	printf("%lf \n", getExpo(num, degr));
-	
-//-----------------------------------------------------------------
-printf("\n");
-//----------------------- Task 3 ----------------------------------
-degr = 0;
-num = 0.0;
-
-printf("ENTER THE NUMBER AND THE DEGREE OF THE NUMBER SEPARATED BY A SPACE: ");
-scanf("%lf %d", &num, &degr);
-
-	printf("%lf \n", expo(num, degr));
-	
-//-----------------------------------------------------------------
-printf("\n");
-//----------------------- Task 4 ----------------------------------
-
-resetArray();
-setImpediment();
-
-	for(int i = 0; i < WIDTH_BOARD; ++i){
-	for(int j = 0; j < HEIGHT_BOARD; ++j){
-		printf("%5d", BOARD[i][j]);
-	}
-	
-	printf("\n");
-}
-printf("\n");
-
-	for(int i = 0; i < WIDTH_BOARD; ++i){
-	
-		for(int j = 0; j < HEIGHT_BOARD; ++j){
+		if(lNode->left != NULL){
 			
-			printf("%5d", stepKing(i, j));
+			lcount++;
 		}
-		printf("\n");
+		else if(lNode->right != NULL){
+			
+			lcount++;
+		}
+		
+	lTree = lTree->left;
+	}
+	
+	
+	while(rTree != NULL){
+		
+		rNode = rTree;
+		
+		if(rNode->right != NULL){
+			
+			rcount++;
+		}
+		else if(rNode->left != NULL){
+			
+			rcount++;
+		}
+		
+	rTree = rTree->right;
 	}
 
+	if((lcount - rcount) > 1 || (lcount - rcount) < -1) return false;
+	else return true;
 
-return 0;
+	}else{
+		
+		return false;
+	}
+	
+}
+
+void printTree(TreeNode *root){
+    if(root){
+        printf("%d", root->key);
+    if(root->left || root->right){
+        printf("(");
+        if(root->left){
+            printTree(root->left);
+        }else{ 
+            printf("NULL");
+         }
+        printf(",");
+        if(root->right){
+            printTree(root->right);
+        }else{
+            printf("NULL");
+        }
+        printf(")");
+    }
+
+    }
+}
+
+TreeNode *searchNode(TreeNode *root, int data){
+	
+	if(root){
+
+		if(root->key != data){
+			
+			if(root->key > data){
+				searchNode(root->left, data);
+			}else{
+				searchNode(root->right, data);
+			}
+		}else{
+			
+			return root;
+		}
+		
+	}else{
+		
+	return NULL;
+	}
+	
+}
+
+void freeMemory(TreeNode *root){
+	
+	if(root){
+		
+		if(root->left)
+			freeMemory(root->left);
+		if(root->right)
+			freeMemory(root->right);	
+						
+	free(root);
+	}
+
+}
+
+int main(const int argc, const char **argv){
+
+//---------------------------- Task 1 ----------------------------	
+TreeNode *tree = NULL;
+
+tree = treeInsert(tree, 10);
+
+treeInsert(tree, 8);
+treeInsert(tree, 19);
+treeInsert(tree, 5);
+treeInsert(tree, 9);
+treeInsert(tree, 20);
+treeInsert(tree, 14);
+
+printTree(tree);
+printf("\n");
+
+printf("Tree is %s Balanced!", isBalanced(tree) ? "" : "not");
+//----------------------------------------------------------------
+printf("\n");
+//----------------------------------------------------------------
+//---------------------------- Task 2 ----------------------------
+
+int const amountTree = 50;
+int const amountNode = 10000;
+TreeNode *forest[amountTree];
+int truthCounter = 0;
+
+	for(int i = 0; i < amountTree; i++){
+		forest[i] = NULL;
+		for(int j = 0; j < amountNode; j++){
+
+			forest[i] = treeInsert(forest[i], rand()%amountNode);
+		}
+
+	}
+	
+	for(int i = 0; i < amountTree; i++){
+		
+			isBalanced(forest[i]) ? truthCounter++ : truthCounter + 0;
+	}
+
+	printf("\n");
+	printf("%f percent Balanced trees!", ((float)amountTree * (float)truthCounter) / 100);
+
+	for(int i = 0; i < amountTree; i++){
+		freeMemory(forest[i]);
+		forest[i] = NULL;
+	}
+
+//----------------------------------------------------------------
+printf("\n");
+//----------------------------------------------------------------
+//---------------------------- Task 3 ----------------------------
+
+TreeNode *treeNode = searchNode(tree, 5);
+
+	if(treeNode != NULL)
+		printf("Desired value %d;", treeNode->key);
+	else
+		printf("Value not found!");
+
+freeMemory(tree);
+treeNode = NULL;
+tree = NULL;
+
+    return 0;
 }
